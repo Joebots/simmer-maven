ddd78b80bb9aba6875834d88375c0ec5f31f9c68
sgologuzov <sergos84@gmail.com>
2017-09-27 12:25:39 +0300

    Added new lines for files, removed unused js file

--- src/main/java/com/joebotics/simmer/client/gui/dialog/ProgrammingDialog.java
+++ src/main/java/com/joebotics/simmer/client/gui/dialog/ProgrammingDialog.java
@@ -2,7 +2,6 @@ package com.joebotics.simmer.client.gui.dialog;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.TextAreaElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.logical.shared.OpenEvent;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -17,52 +16,51 @@ import com.joebotics.simmer.client.gui.widget.TextArea;
 
 import gwt.material.design.client.ui.MaterialIcon;
 import gwt.material.design.client.ui.MaterialModal;
-import gwt.material.design.client.ui.MaterialTextArea;
 
 public class ProgrammingDialog extends Composite {
-
+    
     private static ProgrammingDialogUiBinder uiBinder = GWT.create(ProgrammingDialogUiBinder.class);
-
+    
     interface ProgrammingDialogUiBinder extends UiBinder<Widget, ProgrammingDialog> {
     }
-
+    
     @UiField
     MaterialModal modal;
-
+    
     @UiField
     MaterialIcon btnRun;
-
+    
     @UiField
     MaterialIcon btnDebug;
-
+    
     @UiField
     MaterialIcon btnClose;
-
+    
     @UiField
     MaterialIcon btnExpand;
-
+    
     @UiField
     MaterialIcon btnCollapse;
-
+    
     @UiField
     HTMLPanel blocklyPanel;
-
+    
     @UiField
     HTMLPanel toolboxPanel;
-
+    
     @UiField
     TextArea codeTab;
-
+    
     @UiField
     TextArea consoleTab;
-
+    
     private Bgpio.Params params;
     private Element workspacePlayground;
-
+    
     public ProgrammingDialog() {
         initWidget(uiBinder.createAndBindUi(this));
         blocklyPanel.setHeight("300px");
-
+        
         params = new Bgpio.Params();
         params.media = "lib/blockly/media/";
         params.toolbox = toolboxPanel.getElement().getFirstChildElement();
@@ -70,32 +68,32 @@ public class ProgrammingDialog extends Composite {
         Bgpio.setCodeArea(codeTab);
         Bgpio.setConsoleArea(consoleTab);
     }
-
+    
     @UiHandler("btnRun")
     public void btnRunHandler(ClickEvent event) {
         Bgpio.RunMode.selectMode(1);
         Bgpio.RunMode.run();
     }
-
+    
     @UiHandler("btnCollapse")
     public void btnCollapseHandler(ClickEvent event) {
         modal.setFullscreen(false);
         blocklyPanel.setHeight("300px");
         Bgpio.resize();
     }
-
+    
     @UiHandler("btnExpand")
     public void btnExpandHandler(ClickEvent event) {
         modal.setFullscreen(true);
         blocklyPanel.setHeight("500px");
         Bgpio.resize();
     }
-
+    
     @UiHandler("btnClose")
     public void btnCloseHandler(ClickEvent event) {
         modal.close();
     }
-
+    
     @UiHandler("modal")
     public void modalOpeningHandler(OpenEvent<MaterialModal> event) {
         if (workspacePlayground == null) {
@@ -104,7 +102,7 @@ public class ProgrammingDialog extends Composite {
             Bgpio.setBlocks(DOM.getElementById("startBlocks"));
         }
     }
-
+    
     public void open() {
         modal.open();
     }
