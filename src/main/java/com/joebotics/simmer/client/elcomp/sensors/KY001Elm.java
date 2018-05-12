648d71fa8e96d08fe0a95ec72b844fe22059765e
Eugene Asaulyak <jenya.asaulyak@gmail.com>
2018-05-12 20:02:53 +0300

    Implement task SW-143
    Added celsius and fahrenheit temperature mode for the KY001 module

--- src/main/java/com/joebotics/simmer/client/elcomp/sensors/KY001Elm.java
+++ src/main/java/com/joebotics/simmer/client/elcomp/sensors/KY001Elm.java
@@ -1,17 +1,21 @@
 package com.joebotics.simmer.client.elcomp.sensors;
 
+import com.google.gwt.canvas.dom.client.Context2d;
 import com.joebotics.simmer.client.elcomp.*;
+import com.joebotics.simmer.client.gui.EditInfo;
 import com.joebotics.simmer.client.gui.util.Graphics;
 import com.joebotics.simmer.client.util.StringTokenizer;
 import com.google.gwt.dom.client.ImageElement;
 import com.google.gwt.user.client.ui.Image;
 import com.joebotics.simmer.client.gui.util.Point;
+import gwt.material.design.client.ui.MaterialSwitch;
 
 /**
  * KY-001 Temperature sensor Element
  */
 public class KY001Elm extends ChipElm {
     private final ImageElement thermometer = ImageElement.as(new Image("imgs/components/thermometer.svg").getElement());
+    private boolean celsiusScale = true;
 
     public KY001Elm(int xx, int yy) {
         super(xx, yy);
@@ -37,6 +41,27 @@ public class KY001Elm extends ChipElm {
         return 1;
     }
 
+    @Override
+    public void setEditValue(int n, EditInfo ei) {
+        if (n == 2) {
+            celsiusScale = ei.switchElm.getValue();
+        } else {
+            super.setEditValue(n, ei);
+        }
+    }
+
+    @Override
+    public EditInfo getEditInfo(int n) {
+        if (n == 2) {
+            EditInfo ei = new EditInfo("Scale", 0, -1, -1);
+            ei.switchElm = new MaterialSwitch("Celsius", "Fahrenheit");
+            ei.switchElm.setValue(celsiusScale);
+            return ei;
+        }
+
+        return super.getEditInfo(n);
+    }
+
     public int getDumpType() {
         return 501;
     }
@@ -58,8 +83,18 @@ public class KY001Elm extends ChipElm {
     @Override
     public void draw(Graphics g) {
         Point center = getCenterPoint();
-        g.getContext().drawImage(thermometer, center.getX() - thermometer.getWidth() / 2, center.getY() - thermometer.getHeight() / 2);
+        Pin signalPin = getPins()[0];
 
+        // TODO: Get the temp in a proper way
+        double temperatureValue = signalPin.getCurrent();
+        Context2d context = g.getContext();
+        context.drawImage(thermometer, center.getX() - thermometer.getWidth() / 2, center.getY() - thermometer.getHeight() / 2);
+
+        drawPinValue(g, signalPin, String.valueOf(celsiusScale ? temperatureValue : fahrenheitCelsius(temperatureValue)));
         super.draw(g);
+    }
+
+    private double fahrenheitCelsius(double fahrenheit) {
+        return Math.round(((fahrenheit - 32) * 5) / 9);
     }
 }
